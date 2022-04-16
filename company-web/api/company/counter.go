a462796fd3f90b71960dda538cd9aed7ab243a86
blank <1136589038@qq.com>
2022-04-16 22:53:10 +0800

    feat: company 服务更新 & 下一步服务计划

--- company-web/api/company/counter.go
+++ company-web/api/company/counter.go
@@ -1,3 +1,34 @@
 package company
 
+import (
+	"github.com/gin-gonic/gin"
+	"hr-saas-go/company-web/utils"
+	"net/http"
+)
+
 // 统计服务
+
+func UserCountByCompany(ctx *gin.Context) {
+	// 用户统计
+	ctx.JSON(http.StatusOK, utils.SuccessJson(map[string]int{
+		"count": 1,
+	}))
+}
+
+func DepartmentCount(ctx *gin.Context) {
+	// 部门统计
+	ctx.JSON(http.StatusOK, utils.SuccessJson(map[string]int{
+		"count": 10,
+	}))
+}
+
+func ResumeCount(ctx *gin.Context) {
+	// 简历统计
+	ctx.JSON(http.StatusOK, utils.SuccessJson(map[string]int{
+		"count": 100,
+	}))
+}
+
+func PostCount(ctx *gin.Context) {
+	//岗位统计
+}
